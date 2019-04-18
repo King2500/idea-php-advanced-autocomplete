9f9130d9d6528862f2aaafa239ac4f0184e29a7e
Thomas Schulz <mail@king2500.net>
2019-04-18 03:13:06 +0200

    Fixed HTTP header field completions after colon (#5)

--- src/net/king2500/plugins/PhpAdvancedAutoComplete/PhpFunctionCompletionContributor.java
+++ src/net/king2500/plugins/PhpAdvancedAutoComplete/PhpFunctionCompletionContributor.java
@@ -1,12 +1,14 @@
 package net.king2500.plugins.PhpAdvancedAutoComplete;
 
 import com.intellij.codeInsight.completion.*;
+import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
 import com.intellij.openapi.project.Project;
 import com.intellij.patterns.PlatformPatterns;
 import com.intellij.patterns.PsiElementPattern;
 import com.intellij.psi.PsiElement;
 import com.intellij.util.ProcessingContext;
+import com.jetbrains.php.completion.PhpCompletionUtil;
 import com.jetbrains.php.lang.PhpLanguage;
 import com.jetbrains.php.lang.lexer.PhpTokenTypes;
 import com.jetbrains.php.lang.parser.PhpElementTypes;
@@ -18,7 +20,6 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Arrays;
 
 public class PhpFunctionCompletionContributor extends CompletionContributor {
-    private static final String CARET_MAGIC_IDENTIFIER = "IntellijIdeaRulezzz";
 
     public PhpFunctionCompletionContributor() {
         //noinspection unchecked
@@ -47,7 +48,7 @@ public class PhpFunctionCompletionContributor extends CompletionContributor {
             new CompletionProvider<CompletionParameters>() {
                 public void addCompletions(@NotNull CompletionParameters parameters,
                                            ProcessingContext context,
-                                           @NotNull CompletionResultSet resultSet) {
+                                           @NotNull CompletionResultSet result) {
 
                     String funcName = PhpHelper.getCanonicalFuncName(parameters.getPosition().getParent().getParent().getParent());
                     Project project = parameters.getPosition().getProject();
@@ -58,6 +59,7 @@ public class PhpFunctionCompletionContributor extends CompletionContributor {
                     String[] resultPostfixExceptions = {};
                     boolean resultBold = false;
                     boolean resultCaseSensitivity = true;
+                    InsertHandler<LookupElement> insertHandler = null;
 
                     int paramIndex = PhpHelper.getParameterIndex(parameters.getPosition().getParent());
 
@@ -68,8 +70,8 @@ public class PhpFunctionCompletionContributor extends CompletionContributor {
                     String stringLiteral = parameters.getPosition().getText();
                     String stringPrefix = "";
 
-                    if(stringLiteral.contains(CARET_MAGIC_IDENTIFIER)) {
-                        stringPrefix = stringLiteral.substring(0, stringLiteral.indexOf(CARET_MAGIC_IDENTIFIER));
+                    if(stringLiteral.contains(CompletionUtil.DUMMY_IDENTIFIER)) {
+                        stringPrefix = stringLiteral.substring(0, stringLiteral.indexOf(CompletionUtil.DUMMY_IDENTIFIER));
                     }
 
                     if(methodMatchesAt(funcName, paramIndex, PhpCompletionTokens.iniFuncs, 0)) {
@@ -186,12 +188,12 @@ public class PhpFunctionCompletionContributor extends CompletionContributor {
                         // "1" without trailing space
                         else if (stringPrefix.matches("^[-+]?1$")) {
                             resultElements = PhpCompletionTokens.dateTimeUnits;
-                            resultSet.stopHere();
+                            result.stopHere();
                         }
                         // numbers like 2, 17 without trailing space
                         else if(stringPrefix.matches("^[-+]?\\d+$")) {
                             resultElements = PhpCompletionTokens.dateTimeUnits2;
-                            resultSet.stopHere();
+                            result.stopHere();
                         }
                     }
 
@@ -217,86 +219,97 @@ public class PhpFunctionCompletionContributor extends CompletionContributor {
                         resultElements = PhpCompletionTokens.obHandlerElements;
                     }
 
-                    if(methodMatchesAt(funcName, paramIndex, PhpCompletionTokens.httpHeaderResponseFuncs, 0)) {
-                        if(stringPrefix.startsWith("Allow:")) {
-                            resultElements = PhpCompletionTokens.httpMethods;
-                        }
-                        else if(stringPrefix.startsWith("Accept-Ranges:") || stringPrefix.startsWith("Content-Range:")) {
-                            resultElements = PhpCompletionTokens.httpRangeTypes;
-                        }
-                        else if(stringPrefix.startsWith("Cache-Control:")) {
-                            resultElements = PhpCompletionTokens.httpCacheControlDirectives;
-                        }
-                        else if(stringPrefix.startsWith("Connection:")) {
-                            resultElements = PhpCompletionTokens.httpConnectionOptions;
-                        }
-                        else if(stringPrefix.startsWith("Content-Encoding:")) {
-                            resultElements = PhpCompletionTokens.httpEncodingTokens;
-                            resultCaseSensitivity = false;
-                        }
-                        else if(stringPrefix.startsWith("Content-Language:")) {
-                            resultElements = PhpCompletionTokens.isoLanguageCodes;
-                        }
-                        else if(stringPrefix.startsWith("Content-Location:") || stringPrefix.startsWith("Location:")) {
-                            resultElements = prefixArray("/", FileHelper.getProjectFiles(project));
-                            resultElements = concatArrays(new String[] { "/" }, resultElements);
-                        }
-                        else if(stringPrefix.startsWith("Content-Disposition:")) {
-                            resultElements = PhpCompletionTokens.httpContentDispositionTokens;
-                        }
-                        else if(stringPrefix.startsWith("Content-Type:") && !stringPrefix.contains(";")) {
-                            resultElements = PhpCompletionTokens.mimeTypes;
-                        }
-                        else if(stringPrefix.startsWith("Content-Type:") && stringPrefix.trim().endsWith(";")) {
-                            resultElements = prefixArray("charset=", PhpCompletionTokens.httpCharSets);
-                            resultCaseSensitivity = false;
-                        }
-                        else if(stringPrefix.startsWith("Content-Type:") && stringPrefix.trim().endsWith("charset=")) {
-                            resultElements = PhpCompletionTokens.httpCharSets;
-                            resultCaseSensitivity = false;
-                        }
-                        else if(stringPrefix.startsWith("Pragma:")) {
-                            resultElements = PhpCompletionTokens.httpPragmaDirectives;
-                        }
-                        else if(stringPrefix.startsWith("Proxy-Authenticate:") || stringPrefix.startsWith("WWW-Authenticate:")) {
-                            resultElements = PhpCompletionTokens.httpAuthenticationTypes;
-                        }
-                        else if(stringPrefix.startsWith("Status:") || stringPrefix.startsWith("HTTP/1.0 ") || stringPrefix.startsWith("HTTP/1.1 ")) {
-                            resultElements = PhpCompletionTokens.httpStatusCodes;
+                    boolean allowMultiple = false;
 
-                            if(stringPrefix.startsWith("HTTP/1.1 ")) {
-                                resultElements = concatArrays(resultElements, PhpCompletionTokens.httpStatusCodes11);
-                            }
-                        }
-                        else if(stringPrefix.startsWith("Trailer:")) {
-                            resultElements = PhpCompletionTokens.httpHeaderRequestFields;
-                        }
-                        else if(stringPrefix.startsWith("Transfer-Encoding:")) {
-                            resultElements = PhpCompletionTokens.httpTransferEncodingValues;
-                        }
-                        else if(stringPrefix.startsWith("Vary:")) {
-                            resultElements = concatArrays(new String[] {"*"}, PhpCompletionTokens.httpHeaderRequestFields);
-                        }
-                        else if(stringPrefix.startsWith("X-Frame-Options:")) {
-                            resultElements = PhpCompletionTokens.httpXFrameOptions;
-                        }
-                        else if(stringPrefix.startsWith("Content-Security-Policy:") || stringPrefix.startsWith("X-Content-Security-Policy:") || stringPrefix.startsWith("X-WebKit-CSP:")) {
-                            resultElements = PhpCompletionTokens.httpCSP;
-                        }
-                        else if(stringPrefix.startsWith("X-Content-Type-Options:")) {
-                            resultElements = PhpCompletionTokens.httpXContentTypeOptions;
-                        }
-                        else if(stringPrefix.startsWith("X-UA-Compatible:")) {
-                            resultElements = PhpCompletionTokens.httpXUACompatibleValues;
-                        }
-                        else if(stringPrefix.startsWith("X-Robots-Tag:")) {
-                            resultElements = PhpCompletionTokens.httpXRobotsTagDirectives;
-                        }
-                        else if(!stringPrefix.contains(":")) {
+                    if(methodMatchesAt(funcName, paramIndex, PhpCompletionTokens.httpHeaderResponseFuncs, 0)) {
+                        if(!stringPrefix.contains(":")) {
                             resultElements = PhpCompletionTokens.httpHeaderResponseFields;
                             resultPostfix = ": ";
                             resultPostfixAlt = " ";
                             resultPostfixExceptions = new String[] { "HTTP/1.0", "HTTP/1.1" };
+                            insertHandler = InvokeCompletionInsertHandler.getInstance();
+                        } else {
+                            result = result.withPrefixMatcher(stringPrefix.substring(stringPrefix.indexOf(":") + 2));
+
+                            if(stringPrefix.startsWith("Allow: ")) {
+                                allowMultiple = true;
+                                resultElements = PhpCompletionTokens.httpMethods;
+                            }
+                            else if(stringPrefix.startsWith("Accept-Ranges: ") || stringPrefix.startsWith("Content-Range: ")) {
+                                resultElements = PhpCompletionTokens.httpRangeTypes;
+                            }
+                            else if(stringPrefix.startsWith("Cache-Control: ")) {
+                                allowMultiple = true;
+                                resultElements = PhpCompletionTokens.httpCacheControlDirectives;
+                            }
+                            else if(stringPrefix.startsWith("Connection: ")) {
+                                resultElements = PhpCompletionTokens.httpConnectionOptions;
+                            }
+                            else if(stringPrefix.startsWith("Content-Encoding: ")) {
+                                resultElements = PhpCompletionTokens.httpEncodingTokens;
+                                resultCaseSensitivity = false;
+                            }
+                            else if(stringPrefix.startsWith("Content-Language: ")) {
+                                resultElements = PhpCompletionTokens.isoLanguageCodes;
+                            }
+                            else if(stringPrefix.startsWith("Content-Location: ") || stringPrefix.startsWith("Location: ")) {
+                                resultElements = prefixArray("/", FileHelper.getProjectFiles(project));
+                                resultElements = concatArrays(new String[] { "/" }, resultElements);
+                            }
+                            else if(stringPrefix.startsWith("Content-Disposition: ")) {
+                                resultElements = PhpCompletionTokens.httpContentDispositionTokens;
+                            }
+                            else if(stringPrefix.startsWith("Content-Type: ") && !stringPrefix.contains(";")) {
+                                resultElements = PhpCompletionTokens.mimeTypes;
+                            }
+                            else if(stringPrefix.startsWith("Content-Type: ") && stringPrefix.trim().endsWith(";")) {
+                                resultElements = prefixArray("charset=", PhpCompletionTokens.httpCharSets);
+                                resultCaseSensitivity = false;
+                            }
+                            else if(stringPrefix.startsWith("Content-Type: ") && stringPrefix.trim().endsWith("charset=")) {
+                                resultElements = PhpCompletionTokens.httpCharSets;
+                                resultCaseSensitivity = false;
+                            }
+                            else if(stringPrefix.startsWith("Pragma: ")) {
+                                resultElements = PhpCompletionTokens.httpPragmaDirectives;
+                            }
+                            else if(stringPrefix.startsWith("Proxy-Authenticate: ") || stringPrefix.startsWith("WWW-Authenticate: ")) {
+                                resultElements = PhpCompletionTokens.httpAuthenticationTypes;
+                            }
+                            else if(stringPrefix.startsWith("Status: ") || stringPrefix.startsWith("HTTP/1.0 ") || stringPrefix.startsWith("HTTP/1.1 ")) {
+                                resultElements = PhpCompletionTokens.httpStatusCodes;
+
+                                if(stringPrefix.startsWith("HTTP/1.1 ")) {
+                                    resultElements = concatArrays(resultElements, PhpCompletionTokens.httpStatusCodes11);
+                                }
+                            }
+                            else if(stringPrefix.startsWith("Trailer: ")) {
+                                resultElements = PhpCompletionTokens.httpHeaderRequestFields;
+                            }
+                            else if(stringPrefix.startsWith("Transfer-Encoding: ")) {
+                                resultElements = PhpCompletionTokens.httpTransferEncodingValues;
+                            }
+                            else if(stringPrefix.startsWith("Vary: ")) {
+                                allowMultiple = true;
+                                resultElements = concatArrays(new String[] {"*"}, PhpCompletionTokens.httpHeaderRequestFields);
+                            }
+                            else if(stringPrefix.startsWith("X-Frame-Options: ")) {
+                                resultElements = PhpCompletionTokens.httpXFrameOptions;
+                            }
+                            else if(stringPrefix.startsWith("Content-Security-Policy: ") || stringPrefix.startsWith("X-Content-Security-Policy: ") || stringPrefix.startsWith("X-WebKit-CSP: ")) {
+                                allowMultiple = true;
+                                resultElements = PhpCompletionTokens.httpCSP;
+                            }
+                            else if(stringPrefix.startsWith("X-Content-Type-Options: ")) {
+                                resultElements = PhpCompletionTokens.httpXContentTypeOptions;
+                            }
+                            else if(stringPrefix.startsWith("X-UA-Compatible: ")) {
+                                resultElements = PhpCompletionTokens.httpXUACompatibleValues;
+                            }
+                            else if(stringPrefix.startsWith("X-Robots-Tag: ")) {
+                                allowMultiple = true;
+                                resultElements = PhpCompletionTokens.httpXRobotsTagDirectives;
+                            }
                         }
                     }
 
@@ -310,23 +323,36 @@ public class PhpFunctionCompletionContributor extends CompletionContributor {
 //                        }
 //                    };
 
+                    if (allowMultiple && stringPrefix.contains(", ")) {
+                        result = result.withPrefixMatcher(stringPrefix.substring(stringPrefix.lastIndexOf(", ") + 2));
+                    }
+
                     for(int i=0; i < resultElements.length; i++) {
+
+                        if (allowMultiple && stringPrefix.contains(resultElements[i] + ",")) {
+                            continue;
+                        }
                         String postfix = Arrays.asList(resultPostfixExceptions).contains(resultElements[i]) ? resultPostfixAlt : resultPostfix;
                         LookupElementBuilder builder = LookupElementBuilder.create(resultElements[i] + postfix)
                                 .withCaseSensitivity(resultCaseSensitivity)
                                 .withPresentableText(resultElements[i])
 //                                .withTailText(resultPostfix, true)
                                 .withBoldness(resultBold)
-                                .withLookupString(resultElements[i].toLowerCase())
+                                .withLookupString(resultElements[i].toLowerCase() + postfix)
 //                                .withInsertHandler(handler)
                         ;
 
-                        if(resultInfos.length > 0)
+                        if (resultInfos.length > 0) {
                             builder = builder.withTypeText(resultInfos[i]);
+                        }
+
+                        if (insertHandler != null) {
+                            builder = builder.withInsertHandler(insertHandler);
+                        }
 
                         //LookupElement element = builder.withAutoCompletionPolicy(AutoCompletionPolicy.ALWAYS_AUTOCOMPLETE);
-                        //resultSet.addElement(element);
-                        resultSet.addElement(builder);
+                        //result.addElement(element);
+                        result.addElement(builder);
                     }
                 }
 
@@ -364,5 +390,19 @@ public class PhpFunctionCompletionContributor extends CompletionContributor {
             return true;
 
         return super.invokeAutoPopup(position, typeChar);
+    }
+
+    static class InvokeCompletionInsertHandler implements InsertHandler<LookupElement> {
+
+        private static final InvokeCompletionInsertHandler instance = new InvokeCompletionInsertHandler();
+
+        @Override
+        public void handleInsert(InsertionContext context, LookupElement lookupElement) {
+            PhpCompletionUtil.showCompletion(context);
+        }
+
+        public static InvokeCompletionInsertHandler getInstance() {
+            return instance;
+        }
     }
 }
