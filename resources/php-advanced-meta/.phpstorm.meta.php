8fc17e7159a246ee36fadc9138aa6017e7edbe0d
Thomas Schulz <mail@king2500.net>
2020-03-03 21:24:31 +0100

    Added PhpDoc comment for xAdvancedInjectFileReference meta function (#25)

--- resources/php-advanced-meta/.phpstorm.meta.php
+++ resources/php-advanced-meta/.phpstorm.meta.php
@@ -1,5 +1,14 @@
 <?php
 namespace PHPSTORM_META {
+
+    /**
+     * Adds injection of project file reference in string literals for specified method or function.
+     *
+     * @param mixed       $functionReference Method or function reference, like: \FQN\Foo::bar()
+     * @param int         $argumentIndex     Zero-based index of argument
+     * @param string|null $relativeMode      Either '/' (= from top level), '.' (= from current dir) or '' (auto)
+     * @return string
+     */
     function xAdvancedInjectFileReference($functionReference, $argumentIndex, $relativeMode = null) {
         return "xAdvancedInjectFileReference " . $functionReference . " at " . $argumentIndex . ": " . $relativeMode;
     }
